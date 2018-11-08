edfc37c963b4238df291066aa844084e49561b46
taslimhossain <tasliminfo@gmail.com>
2018-11-08 17:10:45 +0600

    Quick Start

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+function App() {
+  return (
+
+<div className="container">
+      <Typography variant="h2" gutterBottom>Lorem Ipsum is simply dummy text.</Typography>
+      <Typography variant="body1" gutterBottom>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a 
+          type specimen book. It has survived not only five centuries, but also the 
+          leap into electronic typesetting, remaining essentially unchanged. It was 
+          popularised in the 1960s with the release of Letraset sheets containing Lorem
+           Ipsum passages, and more recently with desktop publishing software like Aldus 
+           PageMaker including versions of Lorem Ipsum.
+      </Typography>
+      <Button variant="contained" color="primary"> Read More</Button>
+    </div>
+  );
+}
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
